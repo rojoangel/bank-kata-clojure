b99938367cfd21d864c4510fcad129a99e4289b3
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-23 22:30:43 +0200

    code at the end of the session

--- src/bank/core.clj
+++ src/bank/core.clj
@@ -1,6 +1,23 @@
-(ns bank.core)
+(ns bank.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn init-account []
+  [])
+
+(defn- calculate-balance [bank-account]
+  (if (empty? bank-account)
+    0
+    (:balance (first bank-account))))
+
+(defn deposit [bank-account amount date]
+  (let [previous-balance (calculate-balance bank-account)]
+    (conj bank-account {:date date :amount amount :balance (+ previous-balance amount)})))
+
+(defn withdraw [bank-account amount date]
+  (deposit bank-account (- amount) date))
+
+(defn- print-statement-line [statement-line]
+  (str (:date statement-line) " | " (:amount statement-line) " | " (:balance statement-line)))
+
+(defn print-statement [bank-account]
+  (conj (reverse (map print-statement-line bank-account)) "DATE | AMOUNT | BALANCE"))
--- test/bank/core_test.clj
+++ test/bank/core_test.clj
@@ -2,6 +2,31 @@
   (:require [clojure.test :refer :all]
             [bank.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest statement-print-test
+  (testing "Empty account prints only header"
+    (let [bank-account (init-account)]
+      (is (=
+            ["DATE | AMOUNT | BALANCE"]
+            (print-statement bank-account)))))
+  (testing "Account with deposit prints a statement line"
+    (let [bank-account (deposit (init-account) 99.99 "23/10/2017")]
+      (is (=
+            ["DATE | AMOUNT | BALANCE"
+             "23/10/2017 | 99.99 | 99.99"]
+            (print-statement bank-account)))))
+  (testing "Account with withdraw prints a statement line"
+    (let [bank-account (withdraw (init-account) 99.99 "23/10/2017")]
+      (is (=
+            ["DATE | AMOUNT | BALANCE"
+             "23/10/2017 | -99.99 | -99.99"]
+            (print-statement bank-account)))))
+  (testing "Account with deposit and withdraw prints two statement lines with the actions"
+    (let [bank-account (-> (init-account)
+                           (deposit 100 "23/10/2017")
+                           (withdraw 25 "24/10/2017"))]
+      (is (=
+            ["DATE | AMOUNT | BALANCE"
+             "24/10/2017 | -25 | 75"
+             "23/10/2017 | 100 | 100"]
+            (print-statement bank-account)))))
+  )
